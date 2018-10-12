bb45901c603fa9ea4b2744d31cb73cb2adcf034d
Mattl02 <hoemac16@htl-kaindorf.ac.at>
2018-10-12 12:55:46 +0200

    implemented cellRenderer and tableModel

--- Exercise_202_RadioStations/src/bl/SenderTableModel.java
+++ Exercise_202_RadioStations/src/bl/SenderTableModel.java
@@ -1,10 +1,36 @@
 
 package bl;
 
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
 /**
  *
  * @author Matthias
  */
-public class SenderTableModel {
+public class SenderTableModel extends AbstractTableModel{
+
+    private String[] colNames = {"Sender", "Frequenz", "Band"};
+    private ArrayList<Sender> radios = new ArrayList<>();
+    
+    public void add(Sender s){
+        radios.add(s);
+        this.fireTableRowsInserted(radios.size() - 1, radios.size() - 1);
+    }
+    
+    @Override
+    public int getRowCount() {
+        return radios.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colNames.length;
+    }
+
+    @Override
+    public Object getValueAt(int row, int col) {
+        return radios.get(row);
+    }
 
 }
